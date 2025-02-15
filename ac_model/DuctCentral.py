ac65e49e18346b7ae16ce3ba7de82e6de67bf48c
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2025-02-15 09:13:40 +0900

    Update DuctCentral.py

--- ac_model/DuctCentral.py
+++ ac_model/DuctCentral.py
@@ -1,3 +1,4 @@
+import numpy as np
 import ac_model.main as ac
 from ac_model.calc_refrigerant import get_f_p_sgas, get_f_H_gas_comp_in, get_f_H_gas_comp_out,\
                                       get_f_S_gas, get_f_H_liq, calc_e_ref_H_th
