7b2bef43a2072bbe5ba7611dbb880d0c6fda328d
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2025-02-15 09:19:24 +0900

    Update DuctCentral.py

--- ac_model/DuctCentral.py
+++ ac_model/DuctCentral.py
@@ -306,7 +306,7 @@ class AirconModel(ac.AirconSpec):
                               V_fan_rtd_H, V_fan_mid_H, P_fan_rtd_H, P_fan_mid_H, V_hs_dsgn_H,
                               Theta, h, Theta_hs_in, X_hs_in, q_hs_H, V_hs_supply):
         
-        Theta_hs_out = Theta_hs_in + q_hs_CS / (c_p_air * rho_air * V_hs_supply / 3600)
+        Theta_hs_out = Theta_hs_in + q_hs_H / (c_p_air * rho_air * V_hs_supply / 3600)
         C_df_H = 0.77 if (Theta < 5.0) and (h >= 80.0) else 1.0
 
         q_hs_H = q_hs_H * (1 / C_df_H)
